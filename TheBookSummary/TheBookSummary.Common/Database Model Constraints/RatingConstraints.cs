94c5028b84a8c5957287203407908dd2a1b75893
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-02-06 09:49:06 +0200

    Added Constraints For Rating.cs

---
+++ TheBookSummary/TheBookSummary.Common/Database Model Constraints/RatingConstraints.cs
@@ -0,0 +1,8 @@
+namespace TheBookSummary.Common.Database_Model_Constraints;
+
+public static class RatingConstraints
+{
+    public const int MinStarRating = 0;
+
+    public const int MAxStarRating = 10;
+}
