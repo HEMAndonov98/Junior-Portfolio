b0a746e6aa8e02703fb78e82ae823bfceee98c7b
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-02-17 20:13:54 +0200

    Added missing properties to the view model and removed validation because its unneeded for viewing data

--- TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/BookViewModel.cs
+++ TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/BookViewModel.cs
@@ -1,16 +1,18 @@
+using AutoMapper;
+
 namespace TheBookSummary.Web.ViewModels.Book
 {
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
 
-    using Services.Mapping;
-    
-    using Data.Models.MyBookSummary_Models;
-    using TheBookSummary.Common.ViewModelConstraints;
+    using TheBookSummary.Services.Mapping;
+
+    using TheBookSummary.Data.Models.MyBookSummary_Models;
 
     /// <summary>
     /// Represents a view model for displaying book information.
     /// </summary>
-    public class BookViewModel : IMapFrom<Book>
+    public class BookViewModel : IMapFrom<Book>, IHaveCustomMappings
     {
         /// <summary>
         /// Gets or sets the ID of the book.
@@ -21,16 +23,31 @@ namespace TheBookSummary.Web.ViewModels.Book
         /// Gets or sets the name of the book.
         /// </summary>
         [Display(Name = "Book name")]
-        [Required(ErrorMessage = BookViewModelConstraints.RequiredMessage)]
-        [StringLength(BookViewModelConstraints.BookNameMaxLength,
-            MinimumLength = BookViewModelConstraints.BookNameMinLength,
-            ErrorMessage = BookViewModelConstraints.InvalidNameLength)]
         public string BookName { get; set; }
-        
-        //ToDo AddSummaryViewModel
-        
-        //ToDo AddRatingsViewModel
-        
-        //TODo AddCollectionOfCommentViewModel
+
+        [Display(Name = "Short summary")]
+        public string ShortSummary { get; set; }
+
+        [Display(Name = "Full summary")]
+        public string FullSummary { get; set; }
+
+        [Display(Name = "Rating")]
+        public int StarsRating { get; set; }
+
+        [Display(Name = "Comments")]
+        public ICollection<CommentViewModel> CommentViewModels { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Rating, BookViewModel>()
+                .ForMember(
+                    source => source.StarsRating,
+                    destination => destination.MapFrom(member => member.Stars));
+
+            configuration.CreateMap<Comment[], BookViewModel>()
+                .ForMember(
+                    source => source.CommentViewModels,
+                    destination => destination.MapFrom(member => member));
+        }
     }
 }
