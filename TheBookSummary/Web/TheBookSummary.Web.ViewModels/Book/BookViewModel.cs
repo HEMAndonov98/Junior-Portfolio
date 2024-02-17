be2c71b0d3374b217b2276b6f26130d39e8f4fd1
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-02-17 20:16:40 +0200

    Added documentation to BookViewModel.cs

--- TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/BookViewModel.cs
+++ TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/BookViewModel.cs
@@ -25,18 +25,34 @@ namespace TheBookSummary.Web.ViewModels.Book
         [Display(Name = "Book name")]
         public string BookName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the short summary of the book.
+        /// </summary>
         [Display(Name = "Short summary")]
         public string ShortSummary { get; set; }
 
+        /// <summary>
+        /// Gets or sets the full summary of the book.
+        /// </summary>
         [Display(Name = "Full summary")]
         public string FullSummary { get; set; }
 
+        /// <summary>
+        /// Gets or sets the rating of the book.
+        /// </summary>
         [Display(Name = "Rating")]
         public int StarsRating { get; set; }
 
+        /// <summary>
+        /// Gets or sets the collection of comment view models associated with the book.
+        /// </summary>
         [Display(Name = "Comments")]
         public ICollection<CommentViewModel> CommentViewModels { get; set; }
 
+        /// <summary>
+        /// Custom mapping configuration for the book view model.
+        /// </summary>
+        /// <param name="configuration">The AutoMapper profile configuration.</param>
         public void CreateMappings(IProfileExpression configuration)
         {
             configuration.CreateMap<Rating, BookViewModel>()
