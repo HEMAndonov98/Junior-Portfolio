75bd7ba1d7fa63f36039075389b7fc7d196d38f0
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-02-19 08:22:44 +0200

    Added documentation to CommentViewModel.cs

--- TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/CommentViewModel.cs
+++ TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/CommentViewModel.cs
@@ -1,19 +1,35 @@
-using AutoMapper;
-
 namespace TheBookSummary.Web.ViewModels.Book;
 
 using System.Collections.Generic;
 
+using AutoMapper;
 using TheBookSummary.Data.Models.MyBookSummary_Models;
 using TheBookSummary.Services.Mapping;
 
+/// <summary>
+/// Represents a view model for displaying comment data to the client.
+/// </summary>
 public class CommentViewModel : IMapFrom<Comment>, IHaveCustomMappings
 {
+    /// <summary>
+    /// Gets or sets the text of the comment.
+    /// </summary>
     public string Text { get; set; }
 
+    /// <summary>
+    /// Gets or sets the username of the user who made the comment.
+    /// </summary>
     public string Username { get; set; }
 
+    /// <summary>
+    /// Gets or sets the collection of reply view models associated with the comment.
+    /// </summary>
     public ICollection<CommentViewModel> Replies { get; set; }
+    
+    /// <summary>
+    /// Custom mapping configuration for the comment view model.
+    /// </summary>
+    /// <param name="configuration">The AutoMapper profile configuration.</param>
     public void CreateMappings(IProfileExpression configuration)
     {
         configuration.CreateMap<Comment, CommentViewModel>()
