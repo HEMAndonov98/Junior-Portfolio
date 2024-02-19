a4bfc0b73aa857a206075c61e707f6cf324ddedc
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-02-19 08:17:39 +0200

    Added documentation to BookInputModel.cs

--- TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/BookInputModel.cs
+++ TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/BookInputModel.cs
@@ -7,8 +7,14 @@ using TheBookSummary.Common.ViewModelConstraints;
 using TheBookSummary.Data.Models.MyBookSummary_Models;
 using TheBookSummary.Services.Mapping;
 
+/// <summary>
+/// Represents an input model for creating or updating a book.
+/// </summary>
 public class BookInputModel : IMapTo<Book>
 {
+    /// <summary>
+    /// Gets or sets the name of the book.
+    /// </summary>
     [Display(Name = "Book name")]
     [Required(
         AllowEmptyStrings = false,
@@ -19,9 +25,18 @@ public class BookInputModel : IMapTo<Book>
         ErrorMessage = BookViewModelConstraints.InvalidNameLength)]
     public string BookName { get; set; }
 
+    /// <summary>
+    /// Gets or sets the summary input model for the book.
+    /// </summary>
     public BookSummaryInputModel Summary { get; set; }
 
+    /// <summary>
+    /// Gets or sets the collection of rating input models for the book.
+    /// </summary>
     public ICollection<RatingInputModel> Ratings { get; set; }
 
+    /// <summary>
+    /// Gets or sets the collection of comment input models for the book.
+    /// </summary>
     public ICollection<BookCommentInputModel> Comments { get; set; }
 }
