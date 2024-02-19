abdc3f4ec0250ffb798b100cc17a0cd386eb4803
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-02-19 08:17:21 +0200

    Added Documentation to BookCommentInputModel.cs

--- TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/BookCommentInputModel.cs
+++ TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/BookCommentInputModel.cs
@@ -4,8 +4,14 @@ using System.ComponentModel.DataAnnotations;
 
 using TheBookSummary.Common.Database_Model_Constraints;
 
+/// <summary>
+/// Represents an input model for creating a new comment for a book.
+/// </summary>
 public class BookCommentInputModel
 {
+    /// <summary>
+    /// Gets or sets the text of the comment.
+    /// </summary>
     [Display(Name = "Comment")]
     [Required(
         AllowEmptyStrings = false,
@@ -16,7 +22,13 @@ public class BookCommentInputModel
         ErrorMessage = CommentConstraints.InvalidCommentLength)]
     public string Text { get; set; }
 
+    /// <summary>
+    /// Gets or sets the ID of the book associated with the comment.
+    /// </summary>
     public string BookId { get; set; }
 
+    /// <summary>
+    /// Gets or sets the ID of the user who made the comment.
+    /// </summary>
     public string UserId { get; set; }
 }
