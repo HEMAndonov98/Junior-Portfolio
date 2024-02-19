5cec7ea34382508b043eccfba34e1f7012d0c925
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-02-19 08:22:27 +0200

    Implemented BookSummaryInputModel
    Created a new ViewModel named BookSummaryInputModel to represent input data for adding or updating book summaries. This model includes properties for the short and full summaries of the book, annotated with validation attributes to ensure data integrity. The Display attribute provides user-friendly names for the properties, while the Required and StringLength attributes enforce constraints on the length and presence of the summary text.

--- TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/BookSummaryInputModel.cs
+++ TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/BookSummaryInputModel.cs
@@ -1,6 +1,29 @@
 namespace TheBookSummary.Web.ViewModels.Book;
 
+using System.ComponentModel.DataAnnotations;
+
+using TheBookSummary.Common.ViewModelConstraints;
+
 public class BookSummaryInputModel
 {
-    //Todo Finish Input model
+    [Display(Name = "Short summary")]
+    [Required(
+        AllowEmptyStrings = false,
+        ErrorMessage = BookViewModelConstraints.RequiredMessage)]
+    [StringLength(
+        BookViewModelConstraints.BookShortSummaryMaxLength,
+        MinimumLength = BookViewModelConstraints.BookFullSummaryMinLength,
+        ErrorMessage = BookViewModelConstraints.InvalidSummaryLength)]
+    public string ShortSummary { get; set; }
+
+    [Display(Name = "Full summary")]
+    [Required(
+        AllowEmptyStrings = false,
+        ErrorMessage = BookViewModelConstraints.RequiredMessage)]
+    [StringLength(
+        BookViewModelConstraints.BookFullSummaryMaxLength,
+        MinimumLength = BookViewModelConstraints.BookFullSummaryMinLength,
+        ErrorMessage = BookViewModelConstraints.InvalidSummaryLength)]
+    public string FullSummary { get; set; }
+
 }
