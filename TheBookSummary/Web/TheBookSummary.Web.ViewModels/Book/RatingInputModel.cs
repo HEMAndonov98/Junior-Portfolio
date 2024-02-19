68c760bb63f194adcbe70386591917671fa2d7db
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-02-19 08:23:19 +0200

    Added documentation to RatingInputModel.cs

--- TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/RatingInputModel.cs
+++ TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/RatingInputModel.cs
@@ -6,8 +6,14 @@ using TheBookSummary.Common.Database_Model_Constraints;
 using TheBookSummary.Data.Models.MyBookSummary_Models;
 using TheBookSummary.Services.Mapping;
 
+/// <summary>
+/// Represents an input model for adding a rating to a book.
+/// </summary>
 public class RatingInputModel : IMapTo<Rating>
 {
+    /// <summary>
+    /// Gets or sets the rating given to the book.
+    /// </summary>
     [Display(Name ="Rating")]
     [Required(ErrorMessage = RatingConstraints.RequiredMessage)]
     [Range(
@@ -16,5 +22,8 @@ public class RatingInputModel : IMapTo<Rating>
         ErrorMessage = RatingConstraints.InvalidRatingGiven)]
     public int Stars { get; set; }
 
+    /// <summary>
+    /// Gets or sets the ID of the user who submitted the rating.
+    /// </summary>
     public string UserId { get; set; }
 }
