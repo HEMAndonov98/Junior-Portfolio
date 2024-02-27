67528e15842a14c20f5fe3e82cd7c93431d00b44
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-02-27 19:04:04 +0200

    Updated BookViewModel.cs now the summary is its own separate class view model

--- TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/BookViewModel.cs
+++ TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/BookViewModel.cs
@@ -26,16 +26,9 @@ namespace TheBookSummary.Web.ViewModels.Book
         public string BookName { get; set; }
 
         /// <summary>
-        /// Gets or sets the short summary of the book.
+        /// Gets or sets the summary of the book.
         /// </summary>
-        [Display(Name = "Short summary")]
-        public string ShortSummary { get; set; }
-
-        /// <summary>
-        /// Gets or sets the full summary of the book.
-        /// </summary>
-        [Display(Name = "Full summary")]
-        public string FullSummary { get; set; }
+        public BookSummaryViewModel Summary { get; set; }
 
         /// <summary>
         /// Gets or sets the rating of the book.
@@ -44,26 +37,16 @@ namespace TheBookSummary.Web.ViewModels.Book
         public int StarsRating { get; set; }
 
         /// <summary>
-        /// Gets or sets the collection of comment view models associated with the book.
-        /// </summary>
-        [Display(Name = "Comments")]
-        public ICollection<CommentViewModel> CommentViewModels { get; set; }
-
-        /// <summary>
         /// Custom mapping configuration for the book view model.
         /// </summary>
         /// <param name="configuration">The AutoMapper profile configuration.</param>
         public void CreateMappings(IProfileExpression configuration)
         {
-            configuration.CreateMap<Rating, BookViewModel>()
+            configuration.CreateMap<Book, BookViewModel>()
                 .ForMember(
-                    source => source.StarsRating,
-                    destination => destination.MapFrom(member => member.Stars));
-
-            configuration.CreateMap<Comment[], BookViewModel>()
-                .ForMember(
-                    source => source.CommentViewModels,
-                    destination => destination.MapFrom(member => member));
+                    dst => dst.Summary,
+                    opt => opt.MapFrom(
+                        src => src.BookSummary));
         }
     }
 }
