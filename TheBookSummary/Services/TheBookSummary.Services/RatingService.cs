3fedfc5c3ee07bdcb188eb22eb576d6df07309ba
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-02-28 20:11:52 +0200

    Implemented method for adding Rating to a book
    Not Tested Yet!

--- TheBookSummary/Services/TheBookSummary.Services/RatingService.cs
+++ TheBookSummary/Services/TheBookSummary.Services/RatingService.cs
@@ -1,8 +1,10 @@
 namespace TheBookSummary.Services;
 
+using System.Linq;
 using System.Threading.Tasks;
 
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using TheBookSummary.Data.Common.Repositories;
 using TheBookSummary.Data.Models.MyBookSummary_Models;
 using TheBookSummary.Services.Contracts;
@@ -10,18 +12,35 @@ using TheBookSummary.Web.ViewModels.Book;
 
 public class RatingService : IRatingService
 {
-    private readonly IDeletableEntityRepository<Book> _repository;
+    private readonly IDeletableEntityRepository<Rating> _repository;
     private readonly IMapper _mapper;
 
-    public RatingService(IDeletableEntityRepository<Book> repository, IMapper mapper)
+    public RatingService(IDeletableEntityRepository<Rating> repository, IMapper mapper)
     {
         this._repository = repository;
         this._mapper = mapper;
     }
-    
+
     public async Task AddBookRating(string bookId, RatingInputModel ratingInputModel)
     {
-        
+        var existingRating = await this._repository.AllAsNoTracking()
+            .Include(r => r.Book)
+            .Include(r => r.ApplicationUser)
+            .FirstOrDefaultAsync(r => r.BookId == bookId && r.ApplicationUser.Id == ratingInputModel.UserId);
+
+        if (existingRating != null)
+        {
+             await this.EditBookRating(bookId, ratingInputModel);
+        }
+        else
+        {
+            var rating = this._mapper.Map<Rating>(ratingInputModel);
+
+            await this._repository.AddAsync(rating);
+            await this._repository.SaveChangesAsync();
+
+        }
+
         throw new System.NotImplementedException();
     }
 
