dc774237d72ac2e623e5c341f13969686d90cd1a
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-03-06 13:21:52 +0200

    Created Rating Controller for handling requests related to Rating

---
+++ TheBookSummary/Web/TheBookSummary.Web/Controllers/RatingController.cs
@@ -0,0 +1,30 @@
+﻿namespace TheBookSummary.Web;
+
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using TheBookSummary.Services.Contracts;
+using TheBookSummary.Web.Controllers;
+using TheBookSummary.Web.ViewModels.Book;
+
+public class RatingController : BaseController
+{
+    private readonly IRatingService _ratingService;
+
+    private readonly ILogger _logger;
+
+    public RatingController(IRatingService ratingService, ILogger<RatingController> logger)
+    {
+        this._ratingService = ratingService;
+        this._logger = logger;
+    }
+
+    public async Task<IActionResult> Rate(RatingInputModel ratingInputModel)
+    {
+        await this._ratingService.AddBookRating("1", ratingInputModel);
+
+        return this.Ok();
+    }
+}
