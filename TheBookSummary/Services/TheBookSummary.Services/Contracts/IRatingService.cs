ef717a7178398cf62d1f54f89f554e704cbb8bd0
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-03-07 19:49:35 +0200

    Removed the parameter string bookId form the interface and implementation to fix a problem with the bookId not being passed when the rating form is submitted now everithing is inside the RatingInputModel

--- TheBookSummary/Services/TheBookSummary.Services/Contracts/IRatingService.cs
+++ TheBookSummary/Services/TheBookSummary.Services/Contracts/IRatingService.cs
@@ -6,7 +6,8 @@ using TheBookSummary.Web.ViewModels.Book;
 
 public interface IRatingService
 {
-    Task AddBookRating(string bookId, RatingInputModel ratingInputModel);
+    Task AddBookRating(RatingInputModel ratingInputModel);
 
+    // To do change this to remove the string bookId
     Task EditBookRating(string bookId, RatingInputModel ratingInputModel);
 }
