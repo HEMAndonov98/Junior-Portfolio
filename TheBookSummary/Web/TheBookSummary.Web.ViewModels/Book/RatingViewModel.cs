90c0c9e9d2af274d8563fc3c3d12c10fab423aee
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-03-26 21:38:14 +0200

    Custom Mapping for RatingViewModel.cs

---
+++ TheBookSummary/Web/TheBookSummary.Web.ViewModels/Book/RatingViewModel.cs
@@ -0,0 +1,32 @@
+namespace TheBookSummary.Web.ViewModels.Book;
+
+using AutoMapper;
+using TheBookSummary.Data.Models.MyBookSummary_Models;
+using TheBookSummary.Services.Mapping;
+
+public class RatingViewModel : IMapFrom<Rating>, IHaveCustomMappings
+{
+    /// <summary>
+    /// Gets or sets the rating given to the book.
+    /// </summary>
+    public int Stars { get; set; }
+
+    /// <summary>
+    /// Gets or sets the ID of the Book that is connected to this rating.
+    /// </summary>
+    public string BookId { get; set; }
+
+    /// <inheritdoc />
+    public void CreateMappings(IProfileExpression configuration)
+    {
+        configuration.CreateMap<Rating, RatingViewModel>()
+            .ForMember(
+                dst => dst.Stars,
+                opt => opt.MapFrom(
+                    src => src.Stars))
+            .ForMember(
+                dst => dst.BookId,
+                opt => opt.MapFrom(
+                    src => src.BookId));
+    }
+}
