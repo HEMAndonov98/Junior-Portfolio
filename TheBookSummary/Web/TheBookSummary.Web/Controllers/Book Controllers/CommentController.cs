6a9a5f810b7ecc8f4cbf35b22cbf7761039fce79
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-04-08 20:35:29 +0300

    Basic structure for the Comment Controller

---
+++ TheBookSummary/Web/TheBookSummary.Web/Controllers/Book Controllers/CommentController.cs
@@ -0,0 +1,20 @@
+namespace TheBookSummary.Web.Controllers.Book_Controllers;
+
+using System;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using TheBookSummary.Web.Controllers.Base_Controller;
+
+public class CommentController : BaseController
+{
+    public async Task<IActionResult> AddComment()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IActionResult> AddReply()
+    {
+        throw new NotImplementedException();
+    }
+}
