19dbb018c9394c235dfe50368c5b290e8102e207
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-04-08 20:31:07 +0300

    Implemented an interface for the commenting service

---
+++ TheBookSummary/Services/TheBookSummary.Services/Contracts/ICommentService.cs
@@ -0,0 +1,18 @@
+namespace TheBookSummary.Services.Contracts;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using TheBookSummary.Web.ViewModels.Book;
+using TheBookSummary.Web.ViewModels.Comments;
+
+public interface ICommentService
+{
+     Task AddComment(CommentInputModel inputModel);
+
+     Task AddReplyToComment(CommentInputModel inputModel);
+
+     Task<CommentViewModel> GetSingleComment(string commentId);
+
+     Task<IList<CommentViewModel>> GetCommentSet(int pageNum);
+}
