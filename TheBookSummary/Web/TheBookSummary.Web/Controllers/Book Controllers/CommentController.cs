a416ee7cdcbc17b5e11328eb6c42de215881daf4
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2024-04-10 20:21:20 +0300

    Implemented AddComment Controller method

--- TheBookSummary/Web/TheBookSummary.Web/Controllers/Book Controllers/CommentController.cs
+++ TheBookSummary/Web/TheBookSummary.Web/Controllers/Book Controllers/CommentController.cs
@@ -4,13 +4,45 @@ using System;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using TheBookSummary.Services.Contracts;
 using TheBookSummary.Web.Controllers.Base_Controller;
+using TheBookSummary.Web.ViewModels.Comments;
 
 public class CommentController : BaseController
 {
-    public async Task<IActionResult> AddComment()
+    private readonly ICommentService commentService;
+    private readonly ILogger logger;
+
+    public CommentController(ICommentService commentService, ILogger<CommentController> logger)
     {
-        throw new NotImplementedException();
+        this.commentService = commentService;
+        this.logger = logger;
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> AddComment(CommentInputModel inputModel)
+    {
+        this.ModelState["Username"].Errors.Clear();
+        this.ModelState["ParentCommentId"].Errors.Clear();
+        try
+        {
+            if (!this.ModelState.IsValid)
+            {
+                throw new InvalidOperationException();
+            }
+
+            await this.commentService.AddCommentAsync(inputModel);
+            this.logger.LogInformation($"{this.User.Identity.Name} Added a new comment with text: {Environment.NewLine} {inputModel.Text}");
+            return this.RedirectToAction("Details", "Book", new { id = inputModel.BookId });
+        }
+        catch (Exception e)
+        {
+            this.logger.LogError("CommentController/AddComment", e);
+            return this.RedirectToAction("Details", "Book", new { id = inputModel.BookId });
+        }
     }
 
     public async Task<IActionResult> AddReply()
