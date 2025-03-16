13d0e1e298ed32b34d4ce87501e5dd34a44ebbad
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-03-16 19:59:33 +0200

    added nav link to benefits section

--- personal-portfolio/components/Nav.tsx
+++ personal-portfolio/components/Nav.tsx
@@ -1,38 +1,38 @@
 import React from 'react'
-import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+import { IconHome, IconMoodLookUp, IconUser } from "@tabler/icons-react";
 import { GoProjectRoadmap } from "react-icons/go"
 import { FloatingNav } from './ui/floating-navbar';
 
 
 const navItems = [
-    {
-      name: "Home",
-      link: "/",
-      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "About",
-      link: "#about",
-      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "Projects",
-      link: "#projects",
-      icon: <GoProjectRoadmap className="h-4 w-4 text-neutral-500 dark:text-white" />,
-    },
-    {
-      name: "Contact",
-      link: "/contact",
-      icon: (
-        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
-      ),
-    },
-  ];
+  {
+    name: "Home",
+    link: "/",
+    icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "About",
+    link: "#about",
+    icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Projects",
+    link: "#projects",
+    icon: <GoProjectRoadmap className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Benefits",
+    link: "#benefits",
+    icon: (
+      <IconMoodLookUp className="h-4 w-4 text-neutral-500 dark:text-white" />
+    ),
+  },
+];
 
 const Nav = () => {
   return (
     <nav>
-        <FloatingNav navItems={navItems}/>
+      <FloatingNav navItems={navItems} />
     </nav>
   )
 }
