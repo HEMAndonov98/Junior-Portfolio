606cc9044fd2956563f3a2565468d184ad62bc16
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-03-16 19:59:14 +0200

    Benefit component style improvements

--- personal-portfolio/components/Benefits.tsx
+++ personal-portfolio/components/Benefits.tsx
@@ -4,18 +4,20 @@ import { testimonials } from '@/data'
 
 const Benefits = () => {
     return (
-        <div className='py-20'>
+        <div className='py-15' id='benefits'>
             <h1 className='heading mb-10'>
                 What makes me a great fit for {' '}
                 <span className='text-purple'>
                     your team
                 </span>
             </h1>
-            <div className='flex flex-col items-center justify-center'>
-                <InfiniteMovingCards
-                    items={testimonials}
-                    direction='right'
-                    speed='slow' />
+            <div className='flex flex-col items-center justify-center mb-10'>
+                <div className='h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden'>
+                    <InfiniteMovingCards
+                        items={testimonials}
+                        direction='right'
+                        speed='slow' />
+                </div>
             </div>
         </div>
     )
