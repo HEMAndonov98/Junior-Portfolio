c78ddd85763452c422409842efa17bff5fdec133
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-03-16 19:58:53 +0200

    changed sample rate for sentry to speed up app

--- personal-portfolio/sentry.client.config.ts
+++ personal-portfolio/sentry.client.config.ts
@@ -3,7 +3,6 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
-import { Color } from "three";
 
 Sentry.init({
   dsn: "https://067451c7c704b668355d5fa553d276d2@o4508966657523712.ingest.de.sentry.io/4508966662504528",
@@ -17,10 +16,11 @@ Sentry.init({
   // Define how likely Replay events are sampled.
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
 
   // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+  replaysOnErrorSampleRate: 0.1,
+  
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
--- personal-portfolio/sentry.server.config.ts
+++ personal-portfolio/sentry.server.config.ts
@@ -6,7 +6,8 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://067451c7c704b668355d5fa553d276d2@o4508966657523712.ingest.de.sentry.io/4508966662504528",
-
+  tracesSampleRate: 0.2,
+  replaysSessionSampleRate: 0.1,
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 });
