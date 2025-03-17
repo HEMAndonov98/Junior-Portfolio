2f1472b1b1a9dfcc57c3f87054a0591afa80e16b
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-03-17 10:28:48 +0200

    style improvements for benefits component

--- personal-portfolio/components/Benefits.tsx
+++ personal-portfolio/components/Benefits.tsx
@@ -12,7 +12,7 @@ const Benefits = () => {
                 </span>
             </h1>
             <div className='flex flex-col items-center justify-center mb-10'>
-                <div className='h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden'>
+                <div className='h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden'>
                     <InfiniteMovingCards
                         items={testimonials}
                         direction='right'
