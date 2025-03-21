4d6297e7e072910ad026bd31b1469d8b636c4771
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-03-21 11:33:58 +0200

    Added new component for experience

--- personal-portfolio/app/page.tsx
+++ personal-portfolio/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Benefits from "@/components/Benefits";
 import Education from "@/components/Education";
+import Experience from "@/components/Experience";
 import Grid from "@/components/grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -14,6 +15,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Benefits />
+        <Experience />
         <Education />
       </div>
     </main>
