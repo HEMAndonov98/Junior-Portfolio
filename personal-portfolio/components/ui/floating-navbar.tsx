6f19833bdf011eae1be74fcb259e280704d1c002
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-03-22 15:13:31 +0200

    navigation bar ux style changes

--- personal-portfolio/components/ui/floating-navbar.tsx
+++ personal-portfolio/components/ui/floating-navbar.tsx
@@ -56,7 +56,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-4 py-3  items-center justify-center space-x-4",
+          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-black-300 rounded-2xl dark:bg-[#10132e] bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-6 py-5  items-center justify-center space-x-4",
           className
         )}
       >
@@ -65,7 +65,7 @@ export const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center gap-2 flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+              "relative scale-125 sm:scale-100 dark:text-neutral-50 items-center gap-2 flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500 hover:opacity-65"
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
