21b8d585b74b4b52cf282b2cec688c628efe768c
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-04-05 18:03:49 +0300

    Added UserGroups Migration

---
+++ MyChatRooms/MyChatRooms.API/Migrations/20250405145422_Group and UserGroups entity added.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyChatRooms.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class GroupandUserGroupsentityadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserGroups",
+                columns: table => new
+                {
+                    UserGroupsId = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    GroupId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGroups", x => x.UserGroupsId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChatUserUserGroups",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    UsersId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatUserUserGroups", x => new { x.UserId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_ChatUserUserGroups_AspNetUsers_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ChatUserUserGroups_UserGroups_UserId",
+                        column: x => x.UserId,
+                        principalTable: "UserGroups",
+                        principalColumn: "UserGroupsId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    GroupId = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Groups_UserGroups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "UserGroups",
+                        principalColumn: "UserGroupsId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatUserUserGroups_UsersId",
+                table: "ChatUserUserGroups",
+                column: "UsersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_GroupId",
+                table: "Groups",
+                column: "GroupId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChatUserUserGroups");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "UserGroups");
+        }
+    }
+}
