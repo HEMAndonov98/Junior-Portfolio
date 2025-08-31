ee8acf44b8de595f115685173f389a2ada6dc275
Hristo Emilov Andonov <hristo.andonov98@gmail.com>
2025-08-31 13:25:43 +0300

    Added  new apiUrl

--- Angular App/angular-weather/src/enviorments/enviorment.dev.ts
+++ Angular App/angular-weather/src/enviorments/enviorment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api/weather',
+  production: true,
+  apiUrl: 'https://junior-portfolio-6hrg.onrender.com/api/weather',
 };
